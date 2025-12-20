359da30a04e0ed8412b62c0246bad94b9e9c2de1
Kyle Acuna <134776227+giyugg@users.noreply.github.com>
2025-12-20 21:43:41 +0800

    still ongoing https://youtu.be/-TkoO8Z07hI?t=940

--- mathlesson.cpp
+++ mathlesson.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
 
+int main() {
+
+    int x; //declaration
+    x = 5; //assignment
+    /*
+    Or you could use declaration with assignment using
+    int x = 5;
+    */
+    int y = 6;
+
+    std::cout << x << '\n';
+    std::cout << y << '\n';
+
+    return 0;
+}
