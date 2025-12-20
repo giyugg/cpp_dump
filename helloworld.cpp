5ff5cc27d3b2146857c8be2cf2fdcec984aa5473
Kyle Acuna <134776227+giyugg@users.noreply.github.com>
2025-12-20 21:10:26 +0800

    lesson 1 done! https://www.youtube.com/watch?v=-TkoO8Z07hI

---
+++ helloworld.cpp
@@ -0,0 +1,15 @@
+#include <iostream> // input and output function(?)
+
+int main() {
+
+    // This is a comment!
+    /*
+    This is
+    also ignored!
+    */
+
+    std::cout << "Hello World! ";
+    std::cout << "I like pizza!" << '\n';
+    std::cout << "It's really good!" << std::endl;
+    return 0;
+}
